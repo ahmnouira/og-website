f984b72bb47b231d267fec23d0aa9705aa878d0b
Ahmed Nouria <ahmnouira@gmail.com>
2024-03-04 00:16:48 +0100

    fixed and updated scroll bar style

--- app.vue
+++ app.vue
@@ -8,4 +8,22 @@ body {
   background-color: rgba(29, 29, 33, 255);
   overflow-x: hidden;
 }
+
+::-webkit-scrollbar {
+  width: 2px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+  border-radius: 100px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(229 231 235);
+  border-radius: 100px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(229 231 235);
+}
 </style>
--- components/FeatureCard.vue
+++ components/FeatureCard.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex dark:bg-zinc-900 h-48">
+  <div class="flex dark:bg-zinc-900 h-full">
     <div class="flex flex-col w-full">
       <h4 class="text-2xl font-semibold m-4 text-white">{{ props.title }}</h4>
       <p class="text-gray-700 m-4">{{ props.description }}</p>
